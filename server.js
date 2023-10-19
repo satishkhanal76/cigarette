917f1a90d29287cb376e447b34670e2e2ce6bd77
Satish <satishkhanal76@gmail.com>
2023-10-19 17:44:09 -0400

    added basic routes

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const fs = require("fs");
+const cigarettes = JSON.parse(
+  fs.readFileSync("./data/cigarettes.json", "utf8")
+);
+const discounts = JSON.parse(fs.readFileSync("./data/discounts.json", "utf8"));
+
 app.get("/", (req, res) => {
   res.json({
     messsage: "success",
@@ -9,6 +15,14 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/cigarettes", (req, res) => {
+  res.json(cigarettes);
+});
+
+app.get("/discounts", (req, res) => {
+  res.json(discounts);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App running on ${port}`);
 });
